41732175f421bebb0b8ed7f0251bc4582572da19
MateusDassolerSpezia <163303039+MateusDassolerSpezia@users.noreply.github.com>
2024-08-12 11:07:06 -0300

    Add files via upload

--- java/questao1/Pessoa.java
+++ java/questao1/Pessoa.java
@@ -10,5 +10,40 @@ package questao1;
  * @author mdspezia
  */
 public class Pessoa {
+    private String nome;
+    private double altura;
+    private double peso;
     
+    public double calcularImc() {
+        return peso / (altura * altura);
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    
+    public void setAlura(double altura) {
+        if (altura <= 0) {
+            throw new IllegalArgumentException("Altura deve ser maior que 0");
+        }
+        this.altura = altura;
+    }
+    public void setPeso(double peso) {
+        if (peso <= 0) {
+            throw new IllegalArgumentException("peso deve ser maior que 0");
+        }
+        this.peso = peso;
+    }
+    public void setNome(String nome) {
+        if (nome == null) {
+            throw new IllegalArgumentException("Digite o nome");
+        }
+        this.nome = nome;
+    }
 }
