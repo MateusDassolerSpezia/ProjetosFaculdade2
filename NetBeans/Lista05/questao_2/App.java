7f1242db38586f6553813730789b2d36a6fcdc5e
MateusDassolerSpezia <163303039+MateusDassolerSpezia@users.noreply.github.com>
2024-08-26 01:02:03 -0300

    Add files via upload

---
+++ NetBeans/Lista05/questao_2/App.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package questao_2;
+
+
+/**
+ *
+ * @author Mateus
+ */
+public class App {
+    public App() {
+        
+    }
+    public static void main(String[] args) {
+        new App();
+    }
+}
---
+++ NetBeans/Lista05/questao_2/Quadrante.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package questao_2;
+
+/**
+ *
+ * @author Mateus
+ */
+public enum Quadrante {
+    NENHUM, PRIMEIRO, SEGUNDO, TERCEIRO, QUARTO
+}
