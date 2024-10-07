a2e856cebc023319ef2c07b58b3a62014b0b7651
MateusDassolerSpezia <163303039+MateusDassolerSpezia@users.noreply.github.com>
2024-10-07 11:03:43 -0300

    Add files via upload

---
+++ NetBeans/Lista09/questao1/App.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package questao1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author mdspezia
+ */
+public class App {
+    public App() {
+        Cliente c = new Cliente("Mateus", "Aqui");
+        ContaBancaria b = new ContaBancaria("1234", c, 0);
+        ContaEspecial e = new ContaEspecial(100, "1234", c, 0);
+        
+        e.depositar(20);
+        e.sacar(50);
+        
+        JOptionPane.showMessageDialog(null, e.getLimiteCredito());
+    }
+    public static void main(String[] args) {
+        new App();
+    }
+}
---
+++ NetBeans/Lista09/questao1/ContaEspecial.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package questao1;
+
+/**
+ *
+ * @author mdspezia
+ */
+public class ContaEspecial extends ContaBancaria{
+    
+    private double limiteCredito;
+
+    public ContaEspecial(double limiteCredito, String numero, Cliente titular, double saldo) {
+        super(numero, titular, saldo);
+        setLimiteCredito(limiteCredito);
+    }
+
+    public double getLimiteCredito() {
+        return limiteCredito;
+    }
+
+    public void setLimiteCredito(double limiteCredito) {
+        this.limiteCredito = limiteCredito;
+    }
+    
+    @Override
+    public void sacar(double valor) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException("Valor a ser sacado deve ser maior que 0!");
+        }
+        if (valor > this.limiteCredito) {
+            throw new IllegalArgumentException("Sem limite para esse saque! Saldo: R$ " + getSaldo());
+        }
+        limiteCredito -= valor;
+    }
+     
+}
